c61cd48852744aa9f9763d3a9c7592cdbd98beb6
Nuno <61714135+NunoR1@users.noreply.github.com>
2025-01-14 20:16:37 -0500

    Made some commands

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -23,6 +23,7 @@ public final class Constants
   public static final Matter CHASSIS    = new Matter(new Translation3d(0, 0, Units.inchesToMeters(8)), ROBOT_MASS);
   public static final double LOOP_TIME  = 0.13; //s, 20ms + 110ms sprk max velocity lag
   public static final double MAX_SPEED  = Units.feetToMeters(14.5);
+  public static final double CLIMB_SPEED  = 0.25;
   // Maximum speed of the robot in meters per second, used to limit acceleration.
 
 //  public static final class AutonConstants
@@ -31,6 +32,10 @@ public final class Constants
 //    public static final PIDConstants TRANSLATION_PID = new PIDConstants(0.7, 0, 0);
 //    public static final PIDConstants ANGLE_PID       = new PIDConstants(0.4, 0, 0.01);
 //  }
+
+  // CAN IDs
+  public static final int CLIMBER = 17;
+  public static final int MANIPULATOR = 15;
 
   public static final class DrivebaseConstants
   {
