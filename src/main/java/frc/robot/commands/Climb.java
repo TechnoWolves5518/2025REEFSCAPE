1482af0584273dc765df503db10e270daf76227b
Simon J McLaurin <sjmclaur@ncsu.edu>
2025-01-25 13:47:41 -0500

    Fix swerve offset

--- src/main/java/frc/robot/commands/Climb.java
+++ src/main/java/frc/robot/commands/Climb.java
@@ -7,7 +7,6 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants;
 import frc.robot.subsystems.Climber;
-
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class Climb extends Command {
   /** Creates a new Climb. */
